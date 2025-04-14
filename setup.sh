714ad3ce86ca9dca248718c01a2564e219b21328
rbluhm <richard.bluhm@gmail.com>
2025-04-14 09:47:43 +0200

    added restricted folders

--- setup.sh
+++ setup.sh
@@ -1,11 +1,18 @@
 #!/bin/bash
 
+echo "Creating directories..."
+echo "Manually create ./../data/input"
+echo ".. and also create ./../data/input/restricted and rest is considered public in input"
+
 mkdir data
 mkdir data/intermediate
+mkdir data/intermediate/restricted
 mkdir data/output
+mkdir data/output/restricted
 mkdir output
 mkdir output/figures
 mkdir output/tables
 mkdir output/figures/appendix
 mkdir output/tables/appendix
 mkdir tmp
+mkdir tmp/restricted
--- setup_win.bat
+++ setup_win.bat
@@ -3,11 +3,13 @@
 REM Create directories
 mkdir data
 mkdir data\intermediate
+mkdir data\intermediate\restricted
 mkdir data\output
+mkdir data\output\restricted
 mkdir output
 mkdir output\figures
 mkdir output\tables
 mkdir output\figures\appendix
 mkdir output\tables\appendix
 mkdir tmp
-
+mkdir tmp\restricted
